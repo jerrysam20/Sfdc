79eb8ac9cdec2e37829dd6a9925d1cd2279546d8
jerry.sam <jerry.sam@hpe.com>
2021-02-26 17:36:04 +0530

    changes

--- src/main/frontend/app/index.js
+++ src/main/frontend/app/index.js
@@ -5,11 +5,16 @@ import './App.css';
 import App from './App';
 import registerServiceWorker from './serviceWorker';
 
+
 // TODO: Switch to https://github.com/palmerhq/the-platform#stylesheet when it will be stable
 const styleLink = document.createElement('link');
 styleLink.rel = 'stylesheet';
 styleLink.href =
   'https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css';
+styleLink.href =
+    'https://fonts.googleapis.com/icon?family=Material+Icons';
+
+
 document.head.appendChild(styleLink);
 
 ReactDOM.render(
